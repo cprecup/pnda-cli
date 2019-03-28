6ad2f4016cb0babd032de834ee8980a2b126c9a6
cprecup@cisco.com <cprecup@cisco.com>
2019-03-28 14:28:56 +0100

    Change centos user
    Use centos user instead of cloud-user for Centos7

--- terraform/scripts/authorized_keys.sh
+++ terraform/scripts/authorized_keys.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+
+# Use centos user instead of cloud-user for Centos7
 mkdir /home/centos/.ssh
 cat /tmp/scripts/key_name.pem.pub >> /home/centos/.ssh/authorized_keys
--- terraform/scripts/ssh_conf.sh
+++ terraform/scripts/ssh_conf.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# Use centos user instead of cloud-user for Centos7
 adduser centos
 passwd -d centos
 echo "%centos ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/centos
